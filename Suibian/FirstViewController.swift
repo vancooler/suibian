49d6ae1b26281e459e5dca6199187ac988863caa
Liu, Yang <yang@aglobalway.com>
2016-01-10 09:54:38 -0800

    Initial Commit

---
+++ Suibian/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Suibian
+//
+//  Created by Liu, Yang on 2016-01-10.
+//  Copyright © 2016 Liu, Yang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
